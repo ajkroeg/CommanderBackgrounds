a7fb8bc20a31c1fa64332a96cc2dc9163277f248
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2020-08-30 12:20:18 -0400

    Initial commit

---
+++ CommanderBackgrounds.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Harmony;
+using BattleTech;
+using Unity;
+using HBS.Collections;
+using UnityEngine;
+using BattleTech.UI;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+namespace CommanderBackgrounds
+{
+    public static class Mod
+    {
+        internal static Settings modSettings;
+
+        public static void Init(string modDir, string settings)
+        {
+
+            // read settings
+            try
+            {
+                modSettings = JsonConvert.DeserializeObject<Settings>(settings);
+            }
+            catch (Exception)
+            {
+                modSettings = new Settings();
+            }
+            //            Helpers.PopulateAbilities();
+            var harmony = HarmonyInstance.Create("us.wulfbone.CommanderBackgrounds");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+        public class Settings
+        { }
+
+
+        [HarmonyPatch(typeof(SGCharacterCreationBackgroundSelectionPanel), "Done")]
+        public static class SGCharacterCreationBackgroundSelectionPanel_Done_Patch
+        {
+            public static void Postfix(SGCharacterCreationBackgroundSelectionPanel __instance)
+            {
+                var traverse = Traverse.Create(__instance);
+                var results = new List<SimGameEventResult>();
+                
+                var playerBackground = traverse.Property("playerBackground").GetValue<List<BackgroundDef>>();
+                
+                foreach (BackgroundDef backgroundDef in playerBackground)
+                    {
+                        results.Add(backgroundDef.Results);
+                    }
+
+                SimGameState.ApplySimGameEventResult(results);
+                return;
+            }
+        }
+
+    }
+}
---
+++ CommanderBackgrounds_backup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Harmony;
+using BattleTech;
+using Unity;
+using HBS.Collections;
+using UnityEngine;
+using BattleTech.UI;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Collections.Generic;
+using System.CodeDom;
+
+namespace CommanderBackgrounds_backup
+{
+
+    public static class Mod
+    {
+        public static bool isCharacterCreation = false;
+        internal static Settings modSettings;
+
+        public static void Init(string modDir, string settings)
+        {
+
+            // read settings
+            try
+            {
+                modSettings = JsonConvert.DeserializeObject<Settings>(settings);
+            }
+            catch (Exception)
+            {
+                modSettings = new Settings();
+            }
+            //            Helpers.PopulateAbilities();
+            var harmony = HarmonyInstance.Create("us.wulfbone.CommanderBackgrounds");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+        public class Settings
+        { }
+        [HarmonyPatch(typeof(SimGameState), "OnCareerModeStart")]
+        public static class SimGameState_OnCareerModeStart_Patch
+        {
+            public static void Postfix(SimGameState __instance)
+            {
+                isCharacterCreation = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(SGCharacterCreationBackgroundSelectionPanel), "Done")]
+        public static class SGCharacterCreationBackgroundSelectionPanel_Done_Patch
+        {
+            public static void Postfix(SGCharacterCreationBackgroundSelectionPanel __instance)
+            {
+                isCharacterCreation = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SimGameState), "ApplySimGameResult")]
+        public static class SimGameState_ApplySimGameResult_Patch
+        {
+            public static bool Prefix(SimGameState __instance, SimGameEventResult result, StatCollection stats, TagSet tags)
+            {
+                if (isCharacterCreation == false)
+                {
+                    return true;
+                }
+                else
+                {
+                    SimGameState.ApplySimGameEventResult(result, )
+                    return false;
+                }
+            }
+        }
+
+    }
+}
+}
