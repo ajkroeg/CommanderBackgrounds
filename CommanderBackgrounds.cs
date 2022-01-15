ba56ddc40442268024b0841fb0eb0d945a553279
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2022-01-14 19:01:33 -0500

    allow more start results

--- CommanderBackgrounds.cs
+++ CommanderBackgrounds.cs
@@ -8,33 +8,67 @@ using BattleTech.UI;
 using Newtonsoft.Json;
 using System.Reflection;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
 using UnityEngine.Events;
 
 namespace CommanderBackgrounds
 {
+    public class CustomStartingResult
+    {
+        [JsonIgnore]
+        public List<SimGameEventResult> simResult = new List<SimGameEventResult>();
+        public List<JObject> simResultJOs = new List<JObject>();
+    }
     public static class Mod
     {
+        public static List<SimGameEventResult> StartingResults = new List<SimGameEventResult>();
         internal static Settings modSettings;
-
+        internal static Logger modLog;
         public static void Init(string modDir, string settings)
         {
-
-            // read settings
+            modLog = new Logger(modDir, "backgrounds", true);
             try
-            {
+            { 
                 modSettings = JsonConvert.DeserializeObject<Settings>(settings);
             }
             catch (Exception)
             {
                 modSettings = new Settings();
             }
-            //            Helpers.PopulateAbilities();
             var harmony = HarmonyInstance.Create("us.wulfbone.CommanderBackgrounds");
+            Mod.modLog.LogMessage($"Initializing Commander Backgrounds - Version {typeof(Settings).Assembly.GetName().Version}");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Initialize();
         }
-        public class Settings
-        { }
 
+        public class Settings
+        {
+            public bool enableLogging = true;
+            public CustomStartingResult CustomStartingResults = new CustomStartingResult();
+        }
+
+        public static void Initialize()
+        {
+
+            foreach (var jObject in Mod.modSettings.CustomStartingResults.simResultJOs)
+            {
+                var simResult = new SimGameEventResult();
+                simResult.Scope = jObject["Scope"].ToObject<EventScope>();
+                simResult.Requirements = jObject["Requirements"].ToObject<RequirementDef>();
+                simResult.AddedTags = new TagSet();
+                simResult.AddedTags.FromJSON(jObject["AddedTags"].ToString());
+                simResult.RemovedTags = new TagSet();
+                simResult.RemovedTags.FromJSON(jObject["RemovedTags"].ToString());
+                simResult.Stats = jObject["Stats"].ToObject<SimGameStat[]>();
+                simResult.Actions = jObject["Actions"].ToObject<SimGameResultAction[]>();
+                simResult.ForceEvents = jObject["ForceEvents"].ToObject<SimGameForcedEvent[]>();
+                simResult.TemporaryResult = jObject["TemporaryResult"].ToObject<bool>();
+                simResult.ResultDuration = jObject["ResultDuration"].ToObject<int>();
+                Mod.modSettings.CustomStartingResults.simResult.Add(simResult);
+                Mod.modLog.LogMessage($"Processed a starting simgame result.");
+            }
+        }
 
         [HarmonyPatch(typeof(SGCharacterCreationCareerBackgroundSelectionPanel), "Done")]
         public static class SGCharacterCreationCareerBackgroundSelectionPanel_Done_Patch
@@ -53,6 +87,10 @@ namespace CommanderBackgrounds
                     results.Add(backgroundDef.Results);
                 }
 
+                foreach (var result in Mod.modSettings.CustomStartingResults.simResult)
+                {
+                    results.Add(result);
+                }
                 SimGameState.ApplySimGameEventResult(results);
                 return;
             }
---
+++ Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace CommanderBackgrounds
+{
+    internal class Logger
+    {
+        private static StreamWriter logStreamWriter;
+        private bool enableLogging;
+
+        public Logger(string modDir, string fileName, bool enableLogging)
+        {
+            string filePath = Path.Combine(modDir, $"{fileName}.log");
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            logStreamWriter = File.AppendText(filePath);
+            logStreamWriter.AutoFlush = true;
+
+            this.enableLogging = enableLogging;
+        }
+
+        public void LogMessage(string message)
+        {
+            if (Mod.modSettings.enableLogging)
+            {
+                string ts = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+                logStreamWriter.WriteLine($"INFO: {ts} - {message}");
+            }
+        }
+
+        public static void LogError(string message)
+        {
+            string ts = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+            logStreamWriter.WriteLine($"ERROR: {ts} - {message}");
+        }
+
+        public static void LogException(Exception exception)
+        {
+            string ts = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+            logStreamWriter.WriteLine($"CRITICAL: {ts} - {exception}");
+        }
+    }
+}
